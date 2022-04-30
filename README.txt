542cb53afd9fe9488912b825681f855d8a5d105a
SpikeGR <80254796+SpikeGR-YT@users.noreply.github.com>
2022-04-30 20:46:26 +0300

    Version 1.0

---
+++ README.txt
@@ -0,0 +1,34 @@
+//////////\\\\\\\\\\
+WiFi Password Viewer V1.0
+Created by SpikeGR
+//////////\\\\\\\\\\
+
+Did you forget your WiFi password?
+
+No worries, this program can show you the WiFi password
+easily as long as you already connected to it at least one time
+before, because the way this program works is by looking at the
+saved WiFi which you already entered the password in the past.
+
+WARNING:
+
+DO NOT DELETE/RENAME OR EDIT ANY OF THE FILES. DOING SO MIGHT COMPLETELY BREAK THE PROGRAM!
+YOU HAVE BEEN WARNED!
+
+//////////\\\\\\\\\\
+WiFi Password Viewer V1.0
+Created by SpikeGR
+//////////\\\\\\\\\\
+
+-----
+If you want to get in touch with me, below are my Social Media!
+
+►YouTube:
+youtube.com/spikegreece
+
+►Instagram:
+instagram.com/spikegr_official
+
+►TikTok:
+tiktok.com/@spikegr_yt
+-----
---
+++ WiFiPasswordViewer.bat
@@ -0,0 +1,121 @@
+@echo off
+title WiFi Password Viewer V1.0
+
+:checkforupdates
+echo.
+echo  WiFi Password Viewer V1.0
+echo --------------------------------
+echo  Created by SpikeGR
+echo  YouTube: https://youtube.com/spikegreece
+echo --------------------------------
+echo.
+timeout 3 > nul
+echo Checking for Updates...
+timeout 5 > nul
+set local=1.0
+set localtwo=%local%
+set link=https://pastebin.com/raw/rJkzYy7S
+
+:check
+IF EXIST "version.bat" DEL /Q "version.bat"
+goto :download
+pause
+
+:download
+download %link% version.bat
+CALL "version.bat"
+goto check-2
+
+:check-2
+IF "%local%"=="%localtwo%" goto :yes
+IF NOT "%local%"=="%localtwo%" goto :no
+
+:yes
+cls
+echo.
+echo  WiFi Password Viewer V%localtwo%
+echo --------------------------------
+echo  Created by SpikeGR
+echo  YouTube: https://youtube.com/spikegreece
+echo --------------------------------
+echo.
+echo  No updates found.
+echo  You have the latest update installed.
+timeout 4 > nul
+echo  Windows Free Activation Tool will now start.
+goto start
+
+:no
+cls
+echo.
+echo  WiFi Password Viewer V%localtwo%
+echo --------------------------------
+echo  Created by SpikeGR
+echo  YouTube: https://youtube.com/spikegreece
+echo --------------------------------
+echo.
+echo New Update found!
+echo.
+echo  Version: %local% is available!
+echo  You currently have version %localtwo% installed
+echo.
+echo  Do you want to download the new Update?
+echo  1 - Yes, download the update now.
+echo  2 - No, maybe later.
+echo.
+set /p num="Enter Number: "
+if %num%==1 goto downloadupdate
+if %num%==2 goto start
+
+:downloadupdate
+echo Your browser will now open and download the latest update!
+echo Windows Free Activation Tool will close automatically.
+timeout 5 > nul
+start "" https://rebrand.ly/WiFiPasswordViewer
+exit
+
+:start
+cls
+echo.
+echo  WiFi Password Viewer V%localtwo%
+echo --------------------------------
+echo  Created by SpikeGR
+echo  YouTube: https://youtube.com/spikegreece
+echo --------------------------------
+goto getpass
+
+:getpass
+cls
+echo.
+echo  WiFi Password Viewer V%localtwo%
+echo --------------------------------
+echo  Created by SpikeGR
+echo  YouTube: https://youtube.com/spikegreece
+echo --------------------------------
+echo.
+echo WARNING: "WiFi Password Viewer" DOES NOT steal/crack the password.
+echo           This program finds the password from the saved WiFi which you already connected before!
+echo.
+set /p ssid="Enter SSID Name: "
+cls
+echo.
+echo  WiFi Password Viewer V%localtwo%
+echo --------------------------------
+echo  Created by SpikeGR
+echo  YouTube: https://youtube.com/spikegreece
+echo --------------------------------
+echo.
+echo SSID Name:
+netsh wlan show profile name="%ssid%" key=clear | findstr /a:4 "name"
+echo.
+echo Security mode:
+netsh wlan show profile name="%ssid%" key=clear | findstr /a:4  "Authentication"
+echo.
+echo WiFi Password:
+netsh wlan show profile name="%ssid%" key=clear | findstr /a:4 "Key"
+echo.
+echo If results are empty,  run this program as Administrator and check if you entered SSID name correctly.
+pause>nul
+goto start
+
+:end
